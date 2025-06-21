c5ca2f2ce097d512ffeacb56d32a47c8be3a996d
soligas <38255367+soligas@users.noreply.github.com>
2025-06-21 16:19:32 -0500

    Add next.config.ts 1750540771575

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
   images: {
     remotePatterns: [
       {
@@ -9,6 +11,7 @@ const nextConfig: NextConfig = {
         pathname: '/photos/**',
       },
     ],
+    unoptimized: true,
   },
 }
 
